f64b78a09eff39e58436a7b8d20ebaf1c222530b
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2023-10-26 13:25:36 +0200

    Added configuration option for AD.

--- include/cantera/base/ct_typedefs.h
+++ include/cantera/base/ct_typedefs.h
@@ -1,7 +1,22 @@
 #pragma once
+#ifndef AD_ENABLED
+# define AD_ENABLED 0
+#endif
+
+#if AD_ENABLED
 #include <codi.hpp>
-//using CanteraDouble=codi::RealForward;
-//using CanteraDouble=codi::RealReverse;
-using CanteraDouble=double;
-using CanteraDoublePassive=double;
-#define AD_ENABLED 0
+using CanteraDoublePassive = double;
+
+// Select AD type.
+#if defined(AD_FORWARD)
+using CanteraDouble = codi::RealForward;
+#elif defined(AD_REVERSE)
+using CanteraDouble = codi::RealReverse;
+#else
+#error AD mode configured with unknown type.
+#endif
+
+#else
+using CanteraDoublePassive = double;
+using CanteraDouble = double;
+#endif
